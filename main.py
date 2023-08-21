32f05433279f2cc329e1559857f8515bdd7d4655
Marius <marius1890@yahoo.com>
2023-08-22 01:44:54 +0300

    update

--- main.py
+++ main.py
@@ -7,9 +7,3 @@ param1: dict = {
     'notMinor': 'yes'
 }
 response_post = requests.post(url='https://pixe.la/v1/users', params=param1)
-print(response_post)
-<<<<<<< HEAD
-print('ssddsdsds')
-=======
-print('dsdsds')
->>>>>>> e169bf8322cd6f48a05b1636837270c1be9ae046
