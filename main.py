f753c29e81594d43e146705df77bf650acdf42b9
cadfrunze <marius1890@yahoo.com>
2023-08-26 00:12:57 +0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 pixela_endpoint: str = 'https://pixe.la/v1/users'
 param1: dict = {
@@ -21,6 +22,19 @@ headers_key: dict = {
     'X-USER-TOKEN': param1['token']
 }
 
-graph_endpoint = requests.post(url=f"{pixela_endpoint}/{param1['username']}/graphs",
-                               headers=headers_key, json=param_graph_endpoint)
+# graph_endpoint = requests.post(url=f"{pixela_endpoint}/{param1['username']}/graphs",
+#                                headers=headers_key, json=param_graph_endpoint)
 
+update_graph_param: dict = {
+    'date': f'{datetime.now().year}0{datetime.now().month}{datetime.now().day}',
+    'quantity': '3',
+    # 'optionalData': 'testing'
+}
+
+
+update_graph = requests.post(
+    url=f'{pixela_endpoint}/{param1["username"]}/graphs/{param_graph_endpoint["id"]}',
+    headers=headers_key,
+    json=update_graph_param
+)
+print(update_graph.text)
