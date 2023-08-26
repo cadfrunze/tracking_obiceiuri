8ca64e972dcb203b9374b76f56e98614f6a8e2c8
marius <marius1890@yahoo.com>
2023-08-26 22:19:38 +0300

    update

--- testing.py
+++ testing.py
@@ -1,10 +1,8 @@
-from datetime import datetime
+import datetime
 
-minute = datetime.now().minute
-ora = datetime.now().hour
-zi = datetime.now().day
-luna = datetime.now().month
-an = datetime.now().year
-
+ieri = datetime.date(2023, 8, 25)
+azi = datetime.datetime.now()
+print(ieri.strftime('%d/%m/%y'))
 with open('./work_log.txt', 'a') as fisier:
-    fisier.writelines(f'Data: {zi}/{luna}/{an} | Ora: {ora}:{minute}')
+    fisier.writelines(f'\nData: {ieri.strftime("%d/%m/%y")} | Ora: 19:00')
+    fisier.writelines(f'\nData: {azi.strftime("%d/%m/%y")} | Ora: {azi.hour}:{azi.minute}')
