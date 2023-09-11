950a5722cefe519cd6786e4e747f2b3009730e11
cadfrunze <marius1890@yahoo.com>
2023-09-11 11:23:57 +0300

    update

--- testing.py
+++ testing.py
@@ -1,10 +1,5 @@
 from datetime import datetime
 
-minute = datetime.now().minute
-ora = datetime.now().hour
-zi = datetime.now().day
-luna = datetime.now().month
-an = datetime.now().year
-
+astazi: str = datetime.now().strftime('Data: %d/%m/%Y | Ora: %H:%M')
 with open('./work_log.txt', 'a') as fisier:
-    fisier.writelines(f'Data: {zi}/{luna}/{an} | Ora: {ora}:{minute}')
+    fisier.writelines(f'\n{astazi}')
