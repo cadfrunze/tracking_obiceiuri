b1d04f3225e4ea9a0c476685f254c090730b5468
cadfrunze <marius1890@yahoo.com>
2023-09-11 12:16:35 +0300

    update

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 pixela_endpoint: str = 'https://pixe.la/v1/users'
 param1: dict = {
-    'token': 'testing1985',
-    'username': 'cadfrunze',
+    'token': '****',
+    'username': '*test',
     'agreeTermsOfService': 'yes',
     'notMinor': 'yes'
 }
@@ -24,17 +24,29 @@ headers_key: dict = {
 
 # graph_endpoint = requests.post(url=f"{pixela_endpoint}/{param1['username']}/graphs",
 #                                headers=headers_key, json=param_graph_endpoint)
+astazi: str = datetime(year=2023, month=8, day=27).strftime('%Y%m%d')
+# update_graph_param: dict = {
+#     'date': astazi,
+#     'quantity': '3',
+#     # 'optionalData': 'testing'
+# }
+#
+#
+# update_graph = requests.post(
+#     url=f'{pixela_endpoint}/{param1["username"]}/graphs/{param_graph_endpoint["id"]}',
+#     headers=headers_key,
+#     json=update_graph_param
+# )
+# print(update_graph.text)
 
-update_graph_param: dict = {
-    'date': f'{datetime.now().year}0{datetime.now().month}{datetime.now().day}',
-    'quantity': '3',
-    # 'optionalData': 'testing'
+put_endpoint = f'{pixela_endpoint}/{param1["username"]}/graphs/{param_graph_endpoint["id"]}/{astazi}'
+dict_put: dict = {
+    'quantity': '4'
 }
+cerere_put = requests.put(url=put_endpoint, headers=headers_key, json=dict_put)
+print(cerere_put.text)
 
-
-update_graph = requests.post(
-    url=f'{pixela_endpoint}/{param1["username"]}/graphs/{param_graph_endpoint["id"]}',
-    headers=headers_key,
-    json=update_graph_param
-)
-print(update_graph.text)
+# delete_end_point = /v1/users/<username>/graphs/<graphID>/<yyyyMMdd>
+# delete_endpoint = f'{pixela_endpoint}/{param1["username"]}/graphs/{param_graph_endpoint["id"]}/{astazi}'
+# cerere_delete = requests.delete(url=delete_endpoint, headers=headers_key)
+# print(cerere_delete.text)
